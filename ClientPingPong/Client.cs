651a51bf0b12f5225c4be9af318ee9cc8fd0db29
thelittlecitizen3 <thelittlecitizen3@gmail.com>
2020-09-02 21:35:46 +0300

    added client servers with sockets

---
+++ ClientPingPong/Client.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientPingPong
+{
+    class Client : IClient
+    {
+        Socket SocketConnection;
+        IPAddress ServerIP { get; set; }
+        int ServerPort { get; set; }
+
+        public Client(IPAddress serverIp, int serverPort)
+        {
+            SocketConnection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ServerIP = serverIp;
+            ServerPort = serverPort;
+        }
+        public void Connect()
+        {
+            Console.WriteLine();
+            try
+            {
+                SocketConnection.Connect(ServerIP, ServerPort);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("could not connect server with error:" + ex.Message);
+                throw;
+            }
+            
+        }
+
+        public void PrintData(string data)
+        {
+            Console.WriteLine(data);
+        }
+
+        public void ReadDataFromServer()
+        {
+
+            byte[] buffer = new byte[1024 * 4];
+            int readBytes = SocketConnection.Receive(buffer);
+            string data = Encoding.ASCII.GetString(buffer, 0, readBytes);
+            PrintData(data);
+        }
+
+        public void Run()
+        {
+            Connect();
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter Text To Send: ");
+                    string data = Console.ReadLine();
+                    SendDataToServer(data);
+                    ReadDataFromServer();
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Prolblem With Server..");
+                SocketConnection.Close();
+            }
+            
+        }
+
+        public void SendDataToServer(string Data)
+        {
+            byte[] databytes = Encoding.ASCII.GetBytes(Data);
+            SocketConnection.Send(databytes);
+        }
+
+    }
+}
---
+++ PingPong/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace PingPong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            Console.WriteLine("Enter port to listen");
+            int port = int.Parse(Console.ReadLine());
+            IServer server = new Server(ip, port, 5);
+            server.Run();
+        }
+    }
+}
